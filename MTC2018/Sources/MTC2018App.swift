70dcdee55d7cd949f2dd865b985c60497515d600
Masamichi Ueta <u_masamichi@icloud.com>
2020-07-02 10:40:51 +0900

    use SwiftUI LifeCycle

---
+++ MTC2018/Sources/MTC2018App.swift
@@ -0,0 +1,18 @@
+//
+//  MTC2018App.swift
+//  MTC2018
+//
+//  Created by masamichi on 2020/07/02.
+//  Copyright © 2020 Mercari. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MTC2018App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
